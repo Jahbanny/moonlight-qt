bb02d134f9951d98938ac0746ad2e5537e553ad0
Jahbanny <jahbanny@users.noreply.github.com>
2026-04-21 20:52:18 -0700

    Fix AppImage Wayland plugins and exclude libdrm

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -58,9 +58,10 @@ cp /usr/local/lib/libSDL3.so.0 $DEPLOY_FOLDER/usr/lib/
 
 echo Creating AppImage
 pushd $INSTALLER_FOLDER
+export QT_PLUGIN_PATH=$(qmake6 -query QT_INSTALL_PLUGINS)
 VERSION=$VERSION linuxdeployqt $DEPLOY_FOLDER/usr/share/applications/com.moonlight_stream.Moonlight.desktop \
-  -qmake=qmake6 -qmldir=$SOURCE_ROOT/app/gui -appimage -extra-plugins=tls,wayland-graphics-integration-client,wayland-shell-integration,wayland-decoration-client \
-  -exclude-libs=libwayland-client.so.0,libwayland-cursor.so.0,libwayland-egl.so.1 \
+  -qmake=qmake6 -qmldir=$SOURCE_ROOT/app/gui -appimage -extra-plugins=tls,platforms/libqwayland-generic.so,platforms/libqwayland-egl.so,wayland-graphics-integration-client,wayland-shell-integration,wayland-decoration-client \
+  -exclude-libs=libwayland-client.so.0,libwayland-cursor.so.0,libwayland-egl.so.1,libdrm.so.2 \
   -executable=$DEPLOY_FOLDER/usr/lib/libSDL3.so.0 || fail "linuxdeployqt failed!"
 popd
 
